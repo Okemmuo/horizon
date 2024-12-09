b908fbeaebbd5b526290a06dbbdf7b16ed61d73c
Chibueze Ikenna <francisiyke100@gmail.com>
2024-12-09 13:41:28 +0100

    implement initial file and folder structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
+
+// https://www.figma.com/design/jvcjzjCKw9YlhCNOIY1GPY/Horizon-Banking-App?node-id=2-11&node-type=frame
